faa15a9f7e4d8dc5f8f15565fa97ea9c0ea2203e
Johan Martin <martin.johan@johan-martin.com>
2017-11-09 23:57:19 +0200

    update with latest stylelint config

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
       async: true,
       minChunks: Infinity
     }),
-    new StylelintPlugin({'syntax': 'scss'})
+    new StylelintPlugin({syntax: 'scss', emitErrors: false, lintDirtyModulesOnly: true})
   ],
   module: {
     rules: [
