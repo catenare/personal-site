68781e0b0ae404a1da1d93e640cb0b8e40136df2
Johan Martin <martin.johan@johan-martin.com>
2017-11-10 11:16:22 +0200

    update posts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {Hello} from "./components/home/Hello";
+import {Hello} from "./components/Home/Hello";
 
 import "../scss/app.scss";
 
