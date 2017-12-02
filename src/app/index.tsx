c22e9c0e3547ed7de2b718f02a2fa840e036cfa1
Johan Martin <martin.johan@johan-martin.com>
2017-12-02 12:05:29 +0200

    latest settins

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,22 +3,22 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 // import {list} from "./components/Ingredients/ingredients";
 // import {AddColorForm, colorForm, StatelessForm} from "./components/Ingredients/addcolorform";
-// import {AppRoute} from "./Router";
+import {AppRoute} from "./Router";
 
 // import {SetScroll} from "../lib/utils";
 
-import {StarRating} from "./components/Ingredients/stars"
+// import {StarRating} from "./components/Ingredients/stars"
 
 import "../lib/utils.css";
 import "../scss/app.scss";
 
-// ReactDOM.render(
-//   <AppRoute />,
-//   document.getElementById("root"),
-// );
-
-// ReactDOM.render(colorForm, document.getElementById("react-container"));
 ReactDOM.render(
-  <StarRating totalStars={7} starsSelected={3} />,
+  <AppRoute />,
   document.getElementById("root"),
 );
+
+// ReactDOM.render(colorForm, document.getElementById("react-container"));
+// ReactDOM.render(
+//   <StarRating totalStars={7} starsSelected={3} />,
+//   document.getElementById("root"),
+// );
