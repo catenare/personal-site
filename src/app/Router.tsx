4c901bfd6047dda78fd49efb38d87c855e776599
Johan Martin <martin.johan@johan-martin.com>
2017-12-15 13:05:19 +0200

    convert to tsx all of the js

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,12 +1,34 @@
 import * as React from "react";
 import {HashRouter, Redirect, Route, Switch } from "react-router-dom"; // eslint-disable-line
 
+import {combineReducers, createStore} from "redux";
+import {colors, sort} from "./components/Colors/reducers";
+
+import initialState from "./components/Colors/colors";
+
 import {App} from "./components/Colors/app";
 import {AddColorForm} from "./components/Colors/colorform";
-import {colors} from "./components/Colors/colors";
 import {Hello} from "./components/Home/Hello";
 
 const logColor = (title, color) => console.log(`New Color: ${title} | ${color}`) // tslint:disable-line
+
+const store: any = createStore(
+  combineReducers({colors, sort}),
+  initialState,
+);
+
+console.log(store.getState().colors.length);
+console.log(store.getState().sort);
+
+store.dispatch({
+  color: "#F142FF",
+  id: "2222e1p5-3abl-0p523-30e4-8001l8yf2222",
+  timestamp: "Thu Mar 10 2016 01:11:12 GMT-0800 (PST)",
+  title: "Party Pink",
+  type: "ADD_COLOR",
+});
+
+console.log(store.getState().colors.length);
 
 const AppRoute = (props) => ( // eslint-disable-line
   <HashRouter>
@@ -17,7 +39,7 @@ const AppRoute = (props) => ( // eslint-disable-line
           <AddColorForm onNewColor={logColor} />
         ) } />
         <Route path="/colors" render = {() => (
-          <App colors={colors} />
+          <App colors={initialState} />
         )} />
         <Redirect to="/" />
       </Switch>
