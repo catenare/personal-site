3e5053f9472cac9c0f9ce877edee9b5241bd00c3
Johan Martin <martin.johan@johan-martin.com>
2017-12-18 11:31:30 +0200

    got component to render

--- src/app/components/Redux/ui/users.tsx
+++ src/app/components/Redux/ui/users.tsx
@@ -1,26 +1,20 @@
 import * as React from "react";
 import User from "./user";
 
-// class UsersList extends React.Component<any, any> {
-//   public render (
-//     return {
-
-//     }
-//   )
-// }
-
-const Users = (props) => {
-  const {users} = props;
-  if (users.fetching) {
+class UsersList extends React.Component<any, any> {
+  public render() {
+    const {users} = this.props;
+    if (users.fetching) {
+      return (
+        <h1>Fetching...</h1>
+      );
+    } else {
+    const userList = users.users.map((u, i) => (<User user={u} index={i} />));
     return (
-      <h1>Fetching...</h1>
-    );
-  } else {
-  const userList = users.users.map((u, i) => (<User user={u} index={i} />));
-  return (
-      <ul className="users">{userList}</ul>
-    );
+        <ul className="users">{userList}</ul>
+      );
+    }
   }
-};
+}
 
-export default Users;
+export default UsersList;
