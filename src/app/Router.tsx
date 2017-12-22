29c2b9635baecd0f6ddc4c34493ffba9e9adbf80
Johan Martin <martin.johan@johan-martin.com>
2017-12-22 02:48:54 +0200

    added router

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import {Provider} from "react-redux";
 import {HashRouter, Redirect, Route, Switch } from "react-router-dom"; // eslint-disable-line
+import Posts from "./components/Posts/posts";
 
 const AppRoute = (props) => ( // eslint-disable-line
   <HashRouter>
     <main>
       <Switch>
-        <Route path="/" render = {() => <div>Home Page</div>}/>
+        <Route path="/" render = {() => <Posts />}/>
       </Switch>
     </main>
   </HashRouter>
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import Posts from "./components/Posts/posts";
+// import Posts from "./components/Posts/posts";
+import {AppRoute} from "./Router";
 
 import "../scss/app.scss";
 
 const el = document.getElementById("blog");
 
 ReactDOM.render (
-  <Posts />,
+  <AppRoute />,
   el,
 );
