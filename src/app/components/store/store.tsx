5470c3e10322158025e4f682071bcb33b8553035
Johan Martin <martin.johan@johan-martin.com>
2018-04-03 22:03:53 +0200

    reduce package.json file

--- src/app/components/store/store.tsx
+++ src/app/components/store/store.tsx
@@ -1,6 +1,6 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 // import {composeWithDevTools} from "redux-devtools-extension";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import {captcha} from "../store/reducers/captcha";
 import pages from "../store/reducers/pages";
 import {post, posts} from "../store/reducers/posts";
@@ -9,7 +9,7 @@ import api from "./middleware/api";
 const reducers = combineReducers({pages, post, posts, captcha});
 
 // const composeEnhancers = composeWithDevTools({});
-const middleware = applyMiddleware(logger, api);
+const middleware = applyMiddleware(api);
 
 const store = createStore(reducers, middleware);
 
