b1c23526e9af27fa34c8e393551527510d9f58d5
Johan Martin <martin.johan@johan-martin.com>
2018-04-20 16:48:54 +0200

    menu completed

--- src/app/ts/mainmenu.ts
+++ src/app/ts/mainmenu.ts
@@ -4,23 +4,34 @@ export const showMenu = (selector) => {
     setMenu(selector);
   }
 
-  document.addEventListener( "DOMContentLoaded", () => setMenu(selector), false );
+  document.addEventListener(
+    "DOMContentLoaded", () => setMenu(selector), false
+  );
 };
 
 const setMenu = (selector) => {
   let toggleState = false;
   const menu = document.getElementsByClassName(selector)[0];
-
   menu.className += " hide-menu";
   const baseState = menu.className;
 
-  menu.addEventListener("click", (e) => {
-    if (toggleState) {
-      menu.className = baseState;
+  window.addEventListener(
+    "resize", () => {
       toggleState = false;
-    } else {
-      menu.className += " open-menu";
-      toggleState = true;
-    }
+      setBaseState(menu, baseState, toggleState);
+    },
+  ); // listen to resize
+
+  menu.addEventListener("click", (e) => {
+    toggleState = !toggleState;
+    setBaseState(menu, baseState, toggleState);
   });
+};
+
+const setBaseState = (menu, baseState, toggleState) => {
+  if (toggleState) {
+    menu.className += " open-menu";
+  } else {
+    menu.className = baseState;
+  }
 };
